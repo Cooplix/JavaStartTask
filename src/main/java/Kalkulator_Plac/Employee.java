7a898bbea4b8f2c5593090f95b4a621ce02f0361
Vadym <vadikdeputat@gmail.com>
2020-11-26 16:01:10 +0100

    start new task

---
+++ src/main/java/Kalkulator_Plac/Employee.java
@@ -0,0 +1,13 @@
+package Kalkulator_Plac;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private double payment;
+
+    public Employee(String firstName, String lastName, double payment) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.payment = payment;
+    }
+}
---
+++ src/main/java/Kalkulator_Plac/PaymentCalculator.java
@@ -0,0 +1,14 @@
+package Kalkulator_Plac;
+
+public class PaymentCalculator {
+
+    public double nettoYearPayment() {
+
+
+    }
+
+    public double bruttoYearPayment() {
+
+
+    }
+}
