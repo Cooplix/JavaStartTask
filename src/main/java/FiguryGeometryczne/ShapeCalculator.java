f46c464ef8a63f7b5aa1fd383e7522b0d684c415
Vadym <vadikdeputat@gmail.com>
2020-11-27 14:08:26 +0100

    add test

--- src/main/java/FiguryGeometryczne/ShapeCalculator.java
+++ src/main/java/FiguryGeometryczne/ShapeCalculator.java
@@ -1,4 +1,5 @@
 package FiguryGeometryczne;
 
 public class ShapeCalculator {
+
 }
--- src/test/java/FiguryGeometryczne/ShapeCalculatorTest.java
+++ src/test/java/FiguryGeometryczne/ShapeCalculatorTest.java
@@ -1,7 +1,21 @@
 package FiguryGeometryczne;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class ShapeCalculatorTest extends TestCase {
+public class ShapeCalculatorTest {
+    @Test
+    public void poleProstokataTest() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(6, 0);
+        Point p3 = new Point(6, 5);
+        Point p4 = new Point(0, 5);
+        Rectangle rectangle = new Rectangle(p1, p2, p3, p4);
+        ShapeCalculator shapeCalculator = new ShapeCalculator();
+        double rectArea =  shapeCalculator.rectangleArea(rectangle);
+
+        Assert.assertEquals(30,  rectArea, 0.01);
+    }
 
 }
