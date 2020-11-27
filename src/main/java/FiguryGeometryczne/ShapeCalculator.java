c8bb1ec826685754d8b15c9bbf5913c019268689
Vadym <vadikdeputat@gmail.com>
2020-11-27 14:10:32 +0100

    add method rectangArea and lineLength

--- src/main/java/FiguryGeometryczne/ShapeCalculator.java
+++ src/main/java/FiguryGeometryczne/ShapeCalculator.java
@@ -1,5 +1,20 @@
 package FiguryGeometryczne;
 
 public class ShapeCalculator {
+    public double lineLength(Point start, Point end) {
+        double xPow = Math.pow(end.getX() - start.getX(), 2);
+        double yPow = Math.pow(end.getY() - start.getY(), 2);
+        double xyPowSum = xPow + yPow;
+        return Math.sqrt(xyPowSum);
+    }
 
+    public double rectangleArea(Rectangle rectangle) {
+        Point p1 = rectangle.getP1();
+        Point p2 = rectangle.getP2();
+        Point p3 = rectangle.getP3();
+        Point p4 = rectangle.getP4();
+        double sideA = lineLength(p1, p2);
+        double sideB = lineLength(p1, p4);
+        return sideA * sideB;
+    }
 }
