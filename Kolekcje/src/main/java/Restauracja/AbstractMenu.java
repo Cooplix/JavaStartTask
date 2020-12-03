468753bdde9854ad8c00a9df47e43f6e6fde0de2
Vadym <vadikdeputat@gmail.com>
2020-12-03 17:34:50 +0100

    add AbstractMenu add DrinkMenu

---
+++ Kolekcje/src/main/java/Restauracja/AbstractMenu.java
@@ -0,0 +1,15 @@
+package Restauracja;
+
+import java.util.ArrayList;
+
+public abstract class AbstractMenu <T extends Meal> extends ArrayList<T> {
+    public void showMenu() {
+        showMenuName();
+        for(int i = 0; i <size(); i++) {
+            System.out.println();
+            System.out.println(i + 1 + ". " + get(i).getPrice() + " " + get(i));
+        }
+    }
+
+    protected abstract void showMenuName();
+}
---
+++ Kolekcje/src/main/java/Restauracja/DrinkMenu.java
@@ -0,0 +1,17 @@
+package Restauracja;
+
+public class DrinkMenu extends AbstractMenu {
+
+    DrinkMenu() {
+        add(new Drink("Coca-Cola", 3.20));
+        add(new Drink("Sprite", 3.20));
+        add(new Drink("Fanta", 3.20));
+        add(new Drink("Woda", 0.0));
+    }
+
+    @Override
+    protected void showMenuName() {
+        System.out.println("Menu Napojow");
+        System.out.println("Wybirz napoj");
+    }
+}
